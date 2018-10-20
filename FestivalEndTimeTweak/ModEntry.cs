934c1c30ccda1e282f24a6e5e418963ed6c3cbdd
KirbyLink <kjryan.reeves@gmail.com>
2018-10-19 18:47:13 -0700

    Adding base files

---
+++ FestivalEndTimeTweak/ModEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace FestivalEndTimeTweak
+{
+    class ModEntry
+    {
+    }
+}
